5f1804251b35da5d871ed447674dfd0a7f2f61ca
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-31 17:30:46 +0530

    Add files via upload

---
+++ colorConstants.js
@@ -0,0 +1,3 @@
+const MAJOR_COLOR_NAMES = ["White", "Red", "Black", "Yellow", "Violet"];
+const MINOR_COLOR_NAMES = ["Blue", "Orange", "Green", "Brown", "Slate"];
+module.exports = {MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES}
