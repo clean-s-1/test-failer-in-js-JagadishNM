7c95f0d3cb019ff09004b32c9bf3606ecd1eb7a9
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-31 17:30:11 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -1,8 +1,7 @@
-const {expect} = require('chai')
+const {expect} = require('chai');
+const {MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES} = require("./colorConstants");
 
-function print_color_map() {
-    const majorColors = ["White", "Red", "Black", "Yellow", "Violet"];
-    const minorColors = ["Blue", "Orange", "Green", "Brown", "Slate"];
+function print_color_map(majorColors,minorColors) {
     for (let i = 0; i < majorColors.length; i++) {
         for (let j = 0; j < minorColors.length; j++) {
             console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
@@ -11,6 +10,6 @@ function print_color_map() {
     return majorColors.length * minorColors.length;
 }
 
-result = print_color_map();
+result = print_color_map(MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES);
 expect(result).equals(25);
 console.log('All is well (maybe!)');
