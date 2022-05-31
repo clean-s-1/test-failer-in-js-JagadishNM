449e48373f8cd6ff1636f701d98575332b222656
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-05-31 22:00:36 +0530

    Add files via upload

--- misaligned.js
+++ misaligned.js
@@ -1,15 +1,30 @@
 const {expect} = require('chai');
+const {assert} = require('chai');
 const {MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES} = require("./colorConstants");
 
-function print_color_map(majorColors,minorColors) {
+function get_color_map(majorColors,minorColors) {
+    let colorMap = [];
     for (let i = 0; i < majorColors.length; i++) {
-        for (let j = 0; j < minorColors.length; j++) {
-            console.log(`${i * 5 + j} | ${majorColors[i]} | ${minorColors[j]}`);
-        }
+      for (let j = 0; j < minorColors.length; j++) {
+        colorMap.push({
+          colorCode: i * 5 + j,
+          majorColor: majorColors[i],
+          minorColor: minorColors[j],
+        });
+      }
     }
-    return majorColors.length * minorColors.length;
-}
+    return colorMap;
+  }
+  
+  function print_color_map(colorMaps) {
+    colorMaps.forEach((colorMap) => {
+      console.log(`${colorMap.colorCode} | ${colorMap.majorColor} | ${colorMap.minorColor}`);
+    });
+  }
 
-result = print_color_map(MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES);
-expect(result).equals(25);
+result = get_color_map(MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES);
+print_color_map(result);
+
+expect(result.length).equals(25, 'result length should be 25');
+assert.lengthOf(result, 25);
 console.log('All is well (maybe!)');
