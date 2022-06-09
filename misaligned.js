e9b52b58d1428fa644b86222ef599164cdb4d3eb
JagadishNM <91608364+JagadishNM@users.noreply.github.com>
2022-06-09 17:53:01 +0530

    Update misaligned.js

--- misaligned.js
+++ misaligned.js
@@ -18,7 +18,7 @@ function get_color_map(majorColors,minorColors) {
   
   function print_color_map(colorMaps) {
     colorMaps.forEach((colorMap) => {
-      console.log(`${colorMap.colorCode} | ${colorMap.majorColor} | ${colorMap.minorColor}`);
+      console.log(`${colorMap.colorCode}` + | + `${colorMap.majorColor}` + | + `${colorMap.minorColor}`);
     });
   }
 
@@ -26,5 +26,6 @@ result = get_color_map(MAJOR_COLOR_NAMES, MINOR_COLOR_NAMES);
 print_color_map(result);
 
 expect(result.length).equals(25, 'result length should be 25');
+expect(result[0].colorCode).equals(1, "Color code  start from 1");
 assert.lengthOf(result, 25);
 console.log('All is well (maybe!)');
